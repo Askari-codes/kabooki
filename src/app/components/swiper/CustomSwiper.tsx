5090c7fc1c03218b6e2cf8899a7d8f5621a609a9
askari-fahlyani <askari.fahlyani@gmail.com>
2024-11-28 14:56:52 +0100

    Add: chunk array implemented in Custom Swiper handling more than 5 items in each section

--- src/app/components/swiper/CustomSwiper.tsx
+++ src/app/components/swiper/CustomSwiper.tsx
@@ -2,14 +2,32 @@
 import "swiper/css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperContent from "../SwiperContent";
-import { CustomSwiperProps } from "../../../../models/models";
+import { BookWithWriter,CustomSwiperProps,WriterWithBooks } from "../../../../models/models";
+
+const chunkArray = (array:BookWithWriter[]|WriterWithBooks[], size:number) => {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+  return chunks;
+};
+
+
 
 const CustomSwiper = ({ books, writers }: CustomSwiperProps) => {
+  const isBooks = !!books; 
+  const items = books || writers || []; 
+  const chunkedItems = chunkArray(items, 5);
+  console.log(chunkedItems);
+  
   
   return (
-    <Swiper
+
+    <>
+    {chunkedItems.map((group,index)=>(
+       <Swiper className=""  key={index} 
       spaceBetween={20}
-      slidesPerView={4}
+      slidesPerView={1}
       breakpoints={{
         640: { slidesPerView: 1 },
         768: { slidesPerView: 2 },
@@ -17,21 +35,20 @@ const CustomSwiper = ({ books, writers }: CustomSwiperProps) => {
         1280: { slidesPerView: 4 },
       }}
     >
-      {/* Conditionally render based on props */}
-      {books &&
-        books.map((book) => (
-          <SwiperSlide key={book.id}>
-            <SwiperContent book={book}  />
-          </SwiperSlide>
-        ))}
-
-      {writers &&
-        writers.map((writer) => (
-          <SwiperSlide key={writer.id}>
-            <SwiperContent writer={writer} />
-          </SwiperSlide>
-        ))}
+      {group.map((item)=>(
+        <SwiperSlide className="p-5 "  key={item.id}>
+            <SwiperContent
+            key={isBooks ? (item as BookWithWriter).id : (item as WriterWithBooks).id }
+                      book={isBooks ? (item as BookWithWriter) : undefined}
+                      writer={!isBooks ? (item as WriterWithBooks) : undefined}
+            />
+            
+        </SwiperSlide>
+      ))}
     </Swiper>
+    ))}
+    </>
+    
   );
 };
 
