ceb03a8aebd47bfc0d0ac3dcecc58b557ac5a61b
askari-fahlyani <askari.fahlyani@gmail.com>
2024-11-28 15:26:56 +0100

    Add: BooKProfile page and book/[id] route

---
+++ src/app/api/books/[id]/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../../prisma/client";
+import { error } from "console";
+
+
+
+export async function GET(req:NextRequest,{params}:{params:{id:string}}) {
+    const id = parseInt(params.id)
+    if(isNaN(id))
+        return NextResponse.json('the id is not valid',{status:400})
+
+    try {
+        const book = await prisma.book.findUnique({
+            where:{id},
+            include:{genre:true}
+        })
+        if(!book)
+            return NextResponse.json('there is no such a book')
+    
+        return NextResponse.json(book)
+    } catch (error) {
+       return NextResponse.json(error)
+    }
+
+    
+    
+}
