06d596476a4bb16c0a42f654abd234a486e70663
askari-fahlyani <askari.fahlyani@gmail.com>
2024-12-04 11:54:31 +0100

    connection test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import BookSection from "./books/BookSection";
 import WriterSection from "./writers/WriterSection";
 
 export default function Home() {
+  'git test'
   return (
     <main >
       <WriterSection/>
